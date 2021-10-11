a6e77d9405e251c845b2bda2e81a5a74a980bdd6
Alexandr Vishiakov <cyberlight@live.ru>
2021-10-11 05:37:19 +0000

    Reindent + eslint fix formatting inside index.js

--- index.js
+++ index.js
@@ -1,129 +1,155 @@
-const { readFile } = require('fs');
+/* eslint-disable require-jsdoc */
+const {readFile} = require('fs');
 
 class TagAnalyzer {
-    constructor(tagMetadata) {
-        this.tagMetadata = tagMetadata
-    }
+  constructor(tagMetadata) {
+    this.tagMetadata = tagMetadata;
+  }
 
-    onlyOne(o) {
-        if (Array.isArray(o)) {
-            return o;
-        }
-        return [o];
+  onlyOne(o) {
+    if (Array.isArray(o)) {
+      return o;
     }
+    return [o];
+  }
 
-    zeroOrMore(o) {
-        if (Array.isArray(o)) {
-            return o;
-        }
-        return [o];
+  zeroOrMore(o) {
+    if (Array.isArray(o)) {
+      return o;
     }
+    return [o];
+  }
 
-    or(arr) {
-        return arr.flatMap((item) => {
-            if (item.zeroOrMore) return this.zeroOrMore(item.zeroOrMore);
-            if (item.onlyOne) return this.onlyOne(item.onlyOne);
-        })
+  or(arr) {
+    return arr.flatMap((item) => {
+      if (item.zeroOrMore) return this.zeroOrMore(item.zeroOrMore);
+      if (item.onlyOne) return this.onlyOne(item.onlyOne);
+    });
+  }
+
+  not(o, text) {
+    return o !== text;
+  }
+
+  ifthen(o) {
+    if (Array.isArray(o)) {
+      return o;
     }
+    return [o];
+  }
 
-    not(o, text) {
-        return o !== text;
-    }
-
-    ifthen(o) {
-        if (Array.isArray(o)) {
-            return o;
-        }
-        return [o];
-    }
-
-    ifCond(o, text) {
-      if (o.not) {
-          if (this.not(o, text)) {
-            if (o.then) return this.ifthen(o.then)
-          } else {
-              return [];
-          }
+  ifCond(o, text) {
+    if (o.is) {
+      if (o.is === text) {
+        return this.ifthen(o.then);
       }
-      return [];
+    }
+    if (o.not) {
+      if (this.not(o, text)) {
+        if (o.then) return this.ifthen(o.then);
+      } else {
+        return [];
+      }
+    }
+    return [];
+  }
+
+  defaultCond(o) {
+    if (Array.isArray(o)) {
+      return o;
+    }
+    return [o];
+  }
+
+  getCategories(text) {
+    const {Categories} = this.tagMetadata.rules;
+    const {
+      if: ifCond,
+      default: defaultCond,
+    } = Categories;
+    let tagCategories = [];
+
+    if (ifCond) {
+      tagCategories = tagCategories.concat(this.ifCond(ifCond, text));
     }
 
-    defaultCond(o) {
-        if (Array.isArray(o)) {
-            return o;
-        }
-        return [o];
+    if (defaultCond) {
+      tagCategories = tagCategories.concat(this.defaultCond(defaultCond));
     }
 
-    canIncludeParam(text) {
-        const { ContentModel } = this.tagMetadata.rules;
-        const { 
-            onlyOne, 
-            zeroOrMore,
-            or,
-            if: ifCond,
-            default: defaultCond,
-        } = ContentModel;
+    return [...new Set(tagCategories)];
+  }
 
-        if (onlyOne) {
-            return new Set(this.onlyOne(onlyOne)).has(text);
-        }
-        if (zeroOrMore) {
-            return new Set(this.zeroOrMore(zeroOrMore)).has(text);
-        }
-        if (or) {
-            return new Set(this.or(or)).has(text);
-        }
+  canIncludeParam(text) {
+    const {ContentModel} = this.tagMetadata.rules;
+    const {
+      onlyOne,
+      zeroOrMore,
+      or,
+      if: ifCond,
+      default: defaultCond,
+    } = ContentModel;
 
-        if (ifCond) {
-            return new Set(this.ifCond(ifCond, text)).has(text);
-        }
-
-        if (defaultCond) {
-            return new Set(this.defaultCond(defaultCond)).has(text)
-        }
+    if (onlyOne) {
+      return new Set(this.onlyOne(onlyOne)).has(text);
     }
+
+    if (zeroOrMore) {
+      return new Set(this.zeroOrMore(zeroOrMore)).has(text);
+    }
+
+    if (or) {
+      return new Set(this.or(or)).has(text);
+    }
+
+    if (ifCond) {
+      return new Set(this.ifCond(ifCond, text)).has(text);
+    }
+
+    if (defaultCond) {
+      return new Set(this.defaultCond(defaultCond)).has(text);
+    }
+  }
 }
 
 class CanincludeAnalyzer {
-    constructor() {
+  constructor() {
+    this.controller = new AbortController();
+    this.signal = this.controller.signal;
+    this.loading = false;
+  }
+
+  async load() {
+    return new Promise((resolve, reject) => {
+      if (this.loading) {
+        this.controller.abort();
         this.controller = new AbortController();
-        this.signal = this.controller.signal;
-        this.loading = false;
-    }
+        this.signal = controller.signal;
+      }
+      this.loading = true;
+      const {signal} = this;
+      readFile('./rules.json', {signal}, (err, buf) => {
+        if (err) return reject(err);
+        resolve(JSON.parse(buf));
+      });
+    });
+  }
 
-    async load() {
-        return new Promise((resolve, reject) => {
-            if (this.loading) {
-                this.controller.abort();
-                this.controller = new AbortController();
-                this.signal = controller.signal;
-            }
-            this.loading = true;
-            const { signal } = this;
-            readFile('./rules.json', { signal }, (err, buf) => {
-                if (err) return reject(err);
-                resolve(JSON.parse(buf));
-            });
-        });
-    }
+  analyze(childTagName, parentTagName) {
 
-    analyze(childTagName, parentTagName) {
-
-    }
+  }
 }
 
 if (require.main === module) {
-    const analyzer = new CanincludeAnalyzer();
-    analyzer.load().then((json) => {
-        console.warn("json: ", json);
-    });
+  const analyzer = new CanincludeAnalyzer();
+  analyzer.load().then((json) => {
+    console.warn('json: ', json);
+  });
 } else {
-    module.exports = {
-        TagAnalyzer,
-        CanincludeAnalyzer,
-    }
+  module.exports = {
+    TagAnalyzer,
+    CanincludeAnalyzer,
+  };
 }
-
 
 
