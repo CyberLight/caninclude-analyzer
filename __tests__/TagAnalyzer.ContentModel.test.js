fb0e9d7459d102c383c86d4ecdcf29aa08ec0e89
Alexandr Vishiakov <cyberlight@live.ru>
2021-10-11 05:33:28 +0000

    Reindent all tests for TagAnalyser ContentModel

--- __tests__/TagAnalyzer.ContentModel.test.js
+++ __tests__/TagAnalyzer.ContentModel.test.js
@@ -1,95 +1,95 @@
-const { TagAnalyzer } = require('../index.js')
+const {TagAnalyzer} = require('../index.js');
 
 describe('TagAnalyzer::ContentModel', () => {
-    it.each`
+  it.each`
         parameter              |  expected
         ${'#the-head-element'} |  ${true}
         ${'#the-body-element'} |  ${true}
         ${'#the-h1-element'}   |  ${false}
-    `('Can include $parameter to "html" tag result: $expected', ({ parameter, expected }) => {
-        const tag = {
-            "rules": {
-                "Categories": {},
-                "ContentModel": {
-                    "onlyOne": [
-                        "#the-head-element",
-                        "#the-body-element"
-                    ]
-                }
-            }
-        };
+    `('Can include $parameter to "html" tag result: $expected', ({parameter, expected}) => {
+    const tag = {
+      'rules': {
+        'Categories': {},
+        'ContentModel': {
+          'onlyOne': [
+            '#the-head-element',
+            '#the-body-element',
+          ],
+        },
+      },
+    };
 
-        const analyzer = new TagAnalyzer(tag);
-        expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
-    }); 
+    const analyzer = new TagAnalyzer(tag);
+    expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
+  });
 
-    it.each`
+  it.each`
         parameter                | expected
         ${'#metadata-content-2'} | ${true}
         ${'#the-title-element'}  | ${true}
         ${'#the-base-element'}   | ${true}
         ${'#the-a-element'}      | ${false}
-    `('Can include $parameter to "head" tag result: $expected', ({ parameter, expected }) => {
-        const tag = {
-            "rules": {
-                "Categories": {},
-                "ContentModel": {
-                    "or": [
-                        { "zeroOrMore": "#metadata-content-2" },
-                        { "onlyOne": [
-                            "#the-title-element",
-                            "#the-base-element"
-                        ] }
-                    ]
-                }
-            }
-        };
+    `('Can include $parameter to "head" tag result: $expected', ({parameter, expected}) => {
+    const tag = {
+      'rules': {
+        'Categories': {},
+        'ContentModel': {
+          'or': [
+            {'zeroOrMore': '#metadata-content-2'},
+            {'onlyOne': [
+              '#the-title-element',
+              '#the-base-element',
+            ]},
+          ],
+        },
+      },
+    };
 
-        const analyzer = new TagAnalyzer(tag);
-        expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
-    }); 
+    const analyzer = new TagAnalyzer(tag);
+    expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
+  });
 
-    it.each`
+  it.each`
         parameter                      | expected
         ${'#text-content'}             | ${true}
         ${'#inter-element-whitespace'} | ${false}
         ${'#inter-body-element'}       | ${false}
-    `('Can include $parameter to "title" tag result: $expected', ({ parameter, expected }) => {
-        const tag = {
-            "rules": {
-                "Categories": {
-                    "default": "#metadata-content-2"
-                },
-                "ContentModel": {
-                    "if": {
-                        "not": "#inter-element-whitespace",
-                        "then": "#text-content"
-                    }
-                }
-            }
-        };
+    `('Can include $parameter to "title" tag result: $expected', ({parameter, expected}) => {
+    const tag = {
+      'rules': {
+        'Categories': {
+          'default': '#metadata-content-2',
+        },
+        'ContentModel': {
+          'if': {
+            'not': '#inter-element-whitespace',
+            'then': '#text-content',
+          },
+        },
+      },
+    };
 
-        const analyzer = new TagAnalyzer(tag);
-        expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
-    }); 
+    const analyzer = new TagAnalyzer(tag);
+    expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
+  });
 
-    it.each`
+  it.each`
         parameter                      | expected
         ${'#concept-content-nothing'}  | ${true}
         ${'#inter-body-element'}       | ${false}
-    `('Can include $parameter to "base" tag result: $expected', ({ parameter, expected }) => {
-        const tag = {
-            "rules": {
-                "Categories": {
-                    "default": "#metadata-content-2"
-                },
-                "ContentModel": {
-                    "default": "#concept-content-nothing"
-                }
-            }
-        };
+    `('Can include $parameter to "base" tag result: $expected', ({parameter, expected}) => {
+    const tag = {
+      'rules': {
+        'Categories': {
+          'default': '#metadata-content-2',
+        },
+        'ContentModel': {
+          'default': '#concept-content-nothing',
+        },
+      },
+    };
 
-        const analyzer = new TagAnalyzer(tag);
-        expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
-    }); 
+    const analyzer = new TagAnalyzer(tag);
+    expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
+  });
 });
