c4983bc63d8ef777483724ba40c540cc39a189cc
Alexandr Vishniakov <cyberlight@live.ru>
2021-10-11 10:50:52 +0000

    Added require rules.json + added describe for union of tests for header and footer tags

--- __tests__/TagAnalyzer.Categories.test.js
+++ __tests__/TagAnalyzer.Categories.test.js
@@ -1,4 +1,5 @@
 const {TagAnalyzer} = require('../index.js');
+const rules = require('../rules.json');
 
 describe('TagAnalyzer::Categories', () => {
   it.each`
@@ -6,17 +7,7 @@ describe('TagAnalyzer::Categories', () => {
         ${undefined}                   | ${[]}
         ${'#allowed-in-the-body'}      | ${[]}
     `('Get categoties by $parameter for "html" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {},
-        'ContentModel': {
-          'onlyOne': [
-            '#the-head-element',
-            '#the-body-element',
-          ],
-        },
-      },
-    };
+    const tag = rules.html;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.getCategories(parameter)).toStrictEqual(expected);
@@ -27,20 +18,7 @@ describe('TagAnalyzer::Categories', () => {
         ${undefined}                   | ${[]}
         ${'#allowed-in-the-body'}      | ${[]}
     `('Get categoties by $parameter for "head" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {},
-        'ContentModel': {
-          'or': [
-            {'zeroOrMore': '#metadata-content-2'},
-            {'onlyOne': [
-              '#the-title-element',
-              '#the-base-element',
-            ]},
-          ],
-        },
-      },
-    };
+    const tag = rules.head;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.getCategories(parameter)).toStrictEqual(expected);
@@ -51,19 +29,7 @@ describe('TagAnalyzer::Categories', () => {
         ${undefined}        | ${['#metadata-content-2']}
         ${'#any-option'}    | ${['#metadata-content-2']}
     `('Get categoties by $parameter for "title" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'default': '#metadata-content-2',
-        },
-        'ContentModel': {
-          'if': {
-            'not': '#inter-element-whitespace',
-            'then': '#text-content',
-          },
-        },
-      },
-    };
+    const tag = rules.title;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.getCategories(parameter)).toStrictEqual(expected);
@@ -74,16 +40,7 @@ describe('TagAnalyzer::Categories', () => {
         ${undefined}        | ${['#metadata-content-2']}
         ${'#any-option'}    | ${['#metadata-content-2']}
     `('Get categoties by $parameter for "base" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'default': '#metadata-content-2',
-        },
-        'ContentModel': {
-          'default': '#concept-content-nothing',
-        },
-      },
-    };
+    const tag = rules.base;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.getCategories(parameter)).toStrictEqual(expected);
@@ -94,22 +51,7 @@ describe('TagAnalyzer::Categories', () => {
         ${undefined}                   | ${['#metadata-content-2']}
         ${'#allowed-in-the-body'}      | ${['#flow-content-2', '#phrasing-content-2', '#metadata-content-2']}
     `('Get categoties by $parameter for "link" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'default': '#metadata-content-2',
-          'if': {
-            'is': '#allowed-in-the-body',
-            'then': [
-              '#flow-content-2',
-              '#phrasing-content-2',
-            ],
-          },
-        },
-        'ContentModel': {
-        },
-      },
-    };
+    const tag = rules.link;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.getCategories(parameter)).toStrictEqual(expected);
@@ -120,21 +62,7 @@ describe('TagAnalyzer::Categories', () => {
         ${undefined}                             | ${['#metadata-content-2']}
         ${'#names:-the-itemprop-attribute'}      | ${['#flow-content-2', '#phrasing-content-2', '#metadata-content-2']}
     `('Get categoties by $parameter for "meta" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'default': '#metadata-content-2',
-          'if': {
-            'is': '#names:-the-itemprop-attribute',
-            'then': [
-              '#flow-content-2',
-              '#phrasing-content-2',
-            ],
-          },
-        },
-        'ContentModel': {},
-      },
-    };
+    const tag = rules.meta;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.getCategories(parameter)).toStrictEqual(expected);
@@ -145,16 +73,7 @@ describe('TagAnalyzer::Categories', () => {
         ${undefined}         | ${['#metadata-content-2']}
         ${'#the-a-element'}  | ${['#metadata-content-2']}
     `('Get categoties by $parameter for "style" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'default': '#metadata-content-2',
-        },
-        'ContentModel': {
-          'default': '#text-content',
-        },
-      },
-    };
+    const tag = rules.style;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.getCategories(parameter)).toStrictEqual(expected);
@@ -165,16 +84,7 @@ describe('TagAnalyzer::Categories', () => {
         ${undefined}         | ${['#sectioning-root']}
         ${'#other-content'}  | ${['#sectioning-root']}
     `('Get categoties by $parameter for "body" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'default': '#sectioning-root',
-        },
-        'ContentModel': {
-          'default': '#flow-content-2',
-        },
-      },
-    };
+    const tag = rules.body;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.getCategories(parameter)).toStrictEqual(expected);
@@ -185,20 +95,7 @@ describe('TagAnalyzer::Categories', () => {
         ${undefined}         | ${['#flow-content-2', '#sectioning-content-2', '#palpable-content-2']}
         ${'#other-content'}  | ${['#flow-content-2', '#sectioning-content-2', '#palpable-content-2']}
     `('Get categoties by $parameter for "article" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'any': [
-            '#flow-content-2',
-            '#sectioning-content-2',
-            '#palpable-content-2',
-          ],
-        },
-        'ContentModel': {
-          'default': '#flow-content-2',
-        },
-      },
-    };
+    const tag = rules.article;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.getCategories(parameter)).toStrictEqual(expected);
@@ -209,20 +106,7 @@ describe('TagAnalyzer::Categories', () => {
         ${undefined}         | ${['#flow-content-2', '#sectioning-content-2', '#palpable-content-2']}
         ${'#other-content'}  | ${['#flow-content-2', '#sectioning-content-2', '#palpable-content-2']}
     `('Get categoties by $parameter for "section" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'any': [
-            '#flow-content-2',
-            '#sectioning-content-2',
-            '#palpable-content-2',
-          ],
-        },
-        'ContentModel': {
-          'default': '#flow-content-2',
-        },
-      },
-    };
+    const tag = rules.section;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.getCategories(parameter)).toStrictEqual(expected);
@@ -233,20 +117,7 @@ describe('TagAnalyzer::Categories', () => {
         ${undefined}         | ${['#flow-content-2', '#sectioning-content-2', '#palpable-content-2']}
         ${'#other-content'}  | ${['#flow-content-2', '#sectioning-content-2', '#palpable-content-2']}
     `('Get categoties by $parameter for "nav" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'any': [
-            '#flow-content-2',
-            '#sectioning-content-2',
-            '#palpable-content-2',
-          ],
-        },
-        'ContentModel': {
-          'default': '#flow-content-2',
-        },
-      },
-    };
+    const tag = rules.nav;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.getCategories(parameter)).toStrictEqual(expected);
@@ -257,46 +128,20 @@ describe('TagAnalyzer::Categories', () => {
         ${undefined}         | ${['#flow-content-2', '#sectioning-content-2', '#palpable-content-2']}
         ${'#other-content'}  | ${['#flow-content-2', '#sectioning-content-2', '#palpable-content-2']}
     `('Get categoties by $parameter for "aside" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'any': [
-            '#flow-content-2',
-            '#sectioning-content-2',
-            '#palpable-content-2',
-          ],
-        },
-        'ContentModel': {
-          'default': '#flow-content-2',
-        },
-      },
-    };
+    const tag = rules.aside;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.getCategories(parameter)).toStrictEqual(expected);
   });
 
   describe.each(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])(
-      'For tag=%s', (tag) => {
+      'For tag=%s', (tagName) => {
         it.each`
           parameter            | expected
           ${undefined}         | ${['#flow-content-2', '#heading-content-2', '#palpable-content-2']}
           ${'#other-content'}  | ${['#flow-content-2', '#heading-content-2', '#palpable-content-2']}
-        `(`Get categoties by $parameter for "${tag}" tag result: $expected`, ({parameter, expected}) => {
-          const tag = {
-            'rules': {
-              'Categories': {
-                'any': [
-                  '#flow-content-2',
-                  '#heading-content-2',
-                  '#palpable-content-2',
-                ],
-              },
-              'ContentModel': {
-                'default': '#phrasing-content-2',
-              },
-            },
-          };
+        `(`Get categoties by $parameter for "${tagName}" tag result: $expected`, ({parameter, expected}) => {
+          const tag = rules[tagName];
 
           const analyzer = new TagAnalyzer(tag);
           expect(analyzer.getCategories(parameter)).toStrictEqual(expected);
@@ -308,60 +153,23 @@ describe('TagAnalyzer::Categories', () => {
         ${undefined}         | ${['#flow-content-2', '#heading-content-2', '#palpable-content-2']}
         ${'#other-content'}  | ${['#flow-content-2', '#heading-content-2', '#palpable-content-2']}
     `('Get categoties by $parameter for "hgroup" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'any': [
-            '#flow-content-2',
-            '#heading-content-2',
-            '#palpable-content-2',
-          ],
-        },
-        'ContentModel': {
-          'and': [
-            {'oneOrMore': [
-              '#the-h1,-h2,-h3,-h4,-h5,-and-h6-elements',
-              '#the-h1-element',
-              '#the-h2-element',
-              '#the-h3-element',
-              '#the-h4-element',
-              '#the-h5-element',
-              '#the-h6-element',
-            ]},
-            {'optional': '#script-supporting-elements-2'},
-          ],
-        },
-      },
-    };
+    const tag = rules.hgroup;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.getCategories(parameter)).toStrictEqual(expected);
   });
 
-  it.each`
+  describe.each(['header', 'footer'])(
+      'For tag=%s', (tagName) => {
+        it.each`
         parameter            | expected
         ${undefined}         | ${['#flow-content-2', '#palpable-content-2']}
         ${'#other-content'}  | ${['#flow-content-2', '#palpable-content-2']}
-    `('Get categoties by $parameter for "header" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'default': [
-            '#flow-content-2',
-            '#palpable-content-2',
-          ],
-        },
-        'ContentModel': {
-          'default': '#flow-content-2',
-          'butNo': [
-            '#the-header-element',
-            '#the-footer-element',
-          ],
-        },
-      },
-    };
+    `(`Get categoties by $parameter for "${tagName}" tag result: $expected`, ({parameter, expected}) => {
+          const tag = rules[tagName];
 
-    const analyzer = new TagAnalyzer(tag);
-    expect(analyzer.getCategories(parameter)).toStrictEqual(expected);
-  });
+          const analyzer = new TagAnalyzer(tag);
+          expect(analyzer.getCategories(parameter)).toStrictEqual(expected);
+        });
+      });
 });
--- __tests__/TagAnalyzer.ContentModel.test.js
+++ __tests__/TagAnalyzer.ContentModel.test.js
@@ -1,4 +1,5 @@
 const {TagAnalyzer} = require('../index.js');
+const rules = require('../rules.json');
 
 describe('TagAnalyzer::ContentModel', () => {
   it.each`
@@ -7,17 +8,7 @@ describe('TagAnalyzer::ContentModel', () => {
         ${'#the-body-element'} |  ${true}
         ${'#the-h1-element'}   |  ${false}
     `('Can include $parameter to "html" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {},
-        'ContentModel': {
-          'onlyOne': [
-            '#the-head-element',
-            '#the-body-element',
-          ],
-        },
-      },
-    };
+    const tag = rules.html;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
@@ -30,20 +21,7 @@ describe('TagAnalyzer::ContentModel', () => {
         ${'#the-base-element'}   | ${true}
         ${'#the-a-element'}      | ${false}
     `('Can include $parameter to "head" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {},
-        'ContentModel': {
-          'or': [
-            {'zeroOrMore': '#metadata-content-2'},
-            {'onlyOne': [
-              '#the-title-element',
-              '#the-base-element',
-            ]},
-          ],
-        },
-      },
-    };
+    const tag = rules.head;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
@@ -55,19 +33,7 @@ describe('TagAnalyzer::ContentModel', () => {
         ${'#inter-element-whitespace'} | ${false}
         ${'#inter-body-element'}       | ${false}
     `('Can include $parameter to "title" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'default': '#metadata-content-2',
-        },
-        'ContentModel': {
-          'if': {
-            'not': '#inter-element-whitespace',
-            'then': '#text-content',
-          },
-        },
-      },
-    };
+    const tag = rules.title;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
@@ -78,16 +44,7 @@ describe('TagAnalyzer::ContentModel', () => {
         ${'#concept-content-nothing'}  | ${true}
         ${'#inter-body-element'}       | ${false}
     `('Can include $parameter to "base" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'default': '#metadata-content-2',
-        },
-        'ContentModel': {
-          'default': '#concept-content-nothing',
-        },
-      },
-    };
+    const tag = rules.base;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
@@ -98,22 +55,7 @@ describe('TagAnalyzer::ContentModel', () => {
         ${'#any-content'}  | ${false}
         ${undefined}       | ${false}
     `('Can include $parameter to "link" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'default': '#metadata-content-2',
-          'if': {
-            'is': '#allowed-in-the-body',
-            'then': [
-              '#flow-content-2',
-              '#phrasing-content-2',
-            ],
-          },
-        },
-        'ContentModel': {
-        },
-      },
-    };
+    const tag = rules.link;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
@@ -124,21 +66,7 @@ describe('TagAnalyzer::ContentModel', () => {
         ${'#any-content'}  | ${false}
         ${undefined}       | ${false}
     `('Can include $parameter to "meta" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'default': '#metadata-content-2',
-          'if': {
-            'is': '#names:-the-itemprop-attribute',
-            'then': [
-              '#flow-content-2',
-              '#phrasing-content-2',
-            ],
-          },
-        },
-        'ContentModel': {},
-      },
-    };
+    const tag = rules.meta;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
@@ -150,16 +78,7 @@ describe('TagAnalyzer::ContentModel', () => {
         ${undefined}        | ${false}
         ${'#other-content'} | ${false}
     `('Can include $parameter to "style" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'default': '#metadata-content-2',
-        },
-        'ContentModel': {
-          'default': '#text-content',
-        },
-      },
-    };
+    const tag = rules.style;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
@@ -171,16 +90,7 @@ describe('TagAnalyzer::ContentModel', () => {
         ${undefined}           | ${false}
         ${'#other-content'}    | ${false}
     `('Can include $parameter to "body" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'default': '#sectioning-root',
-        },
-        'ContentModel': {
-          'default': '#flow-content-2',
-        },
-      },
-    };
+    const tag = rules.body;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
@@ -192,20 +102,7 @@ describe('TagAnalyzer::ContentModel', () => {
         ${undefined}           | ${false}
         ${'#other-content'}    | ${false}
     `('Can include $parameter to "article" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'any': [
-            '#flow-content-2',
-            '#sectioning-content-2',
-            '#palpable-content-2',
-          ],
-        },
-        'ContentModel': {
-          'default': '#flow-content-2',
-        },
-      },
-    };
+    const tag = rules.article;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
@@ -217,20 +114,7 @@ describe('TagAnalyzer::ContentModel', () => {
         ${undefined}           | ${false}
         ${'#other-content'}    | ${false}
     `('Can include $parameter to "section" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'any': [
-            '#flow-content-2',
-            '#sectioning-content-2',
-            '#palpable-content-2',
-          ],
-        },
-        'ContentModel': {
-          'default': '#flow-content-2',
-        },
-      },
-    };
+    const tag = rules.section;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
@@ -242,20 +126,7 @@ describe('TagAnalyzer::ContentModel', () => {
         ${undefined}           | ${false}
         ${'#other-content'}    | ${false}
     `('Can include $parameter to "nav" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'any': [
-            '#flow-content-2',
-            '#sectioning-content-2',
-            '#palpable-content-2',
-          ],
-        },
-        'ContentModel': {
-          'default': '#flow-content-2',
-        },
-      },
-    };
+    const tag = rules.nav;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
@@ -267,47 +138,21 @@ describe('TagAnalyzer::ContentModel', () => {
         ${undefined}           | ${false}
         ${'#other-content'}    | ${false}
     `('Can include $parameter to "aside" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'any': [
-            '#flow-content-2',
-            '#sectioning-content-2',
-            '#palpable-content-2',
-          ],
-        },
-        'ContentModel': {
-          'default': '#flow-content-2',
-        },
-      },
-    };
+    const tag = rules.aside;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
   });
 
   describe.each(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])(
-      'For tag=%s', (tag) => {
+      'For tag=%s', (tagName) => {
         it.each`
           parameter                 | expected
           ${'#phrasing-content-2'}  | ${true}
           ${undefined}              | ${false}
           ${'#flow-content-2'}      | ${false}
-        `(`Can include $parameter to "${tag}" tag result: $expected`, ({parameter, expected}) => {
-          const tag = {
-            'rules': {
-              'Categories': {
-                'any': [
-                  '#flow-content-2',
-                  '#heading-content-2',
-                  '#palpable-content-2',
-                ],
-              },
-              'ContentModel': {
-                'default': '#phrasing-content-2',
-              },
-            },
-          };
+        `(`Can include $parameter to "${tagName}" tag result: $expected`, ({parameter, expected}) => {
+          const tag = rules[tagName];
 
           const analyzer = new TagAnalyzer(tag);
           expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
@@ -327,63 +172,26 @@ describe('TagAnalyzer::ContentModel', () => {
         ${undefined}                                   | ${false}
         ${'#other-content'}                            | ${false}
     `('Can include $parameter to "hgroup" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'any': [
-            '#flow-content-2',
-            '#heading-content-2',
-            '#palpable-content-2',
-          ],
-        },
-        'ContentModel': {
-          'and': [
-            {'oneOrMore': [
-              '#the-h1,-h2,-h3,-h4,-h5,-and-h6-elements',
-              '#the-h1-element',
-              '#the-h2-element',
-              '#the-h3-element',
-              '#the-h4-element',
-              '#the-h5-element',
-              '#the-h6-element',
-            ]},
-            {'optional': '#script-supporting-elements-2'},
-          ],
-        },
-      },
-    };
+    const tag = rules.hgroup;
 
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
   });
 
-  it.each`
+  describe.each(['header', 'footer'])(
+      'For tag=%s', (tagName) => {
+        it.each`
         parameter                | expected
         ${'#flow-content-2'}     | ${true}
         ${undefined}             | ${false}
         ${'#the-header-element'} | ${false}
         ${'#the-footer-element'} | ${false}
         ${'#other-content'}      | ${false}
-    `('Can include $parameter to "header" tag result: $expected', ({parameter, expected}) => {
-    const tag = {
-      'rules': {
-        'Categories': {
-          'default': [
-            '#flow-content-2',
-            '#palpable-content-2',
-          ],
-        },
-        'ContentModel': {
-          'default': '#flow-content-2',
-          'butNo': [
-            '#the-header-element',
-            '#the-footer-element',
-          ],
-        },
-      },
-    };
+    `(`Can include $parameter to "${tagName}" tag result: $expected`, ({parameter, expected}) => {
+          const tag = rules[tagName];
 
-    const analyzer = new TagAnalyzer(tag);
-    expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
-  });
+          const analyzer = new TagAnalyzer(tag);
+          expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
+        });
+      });
 });
