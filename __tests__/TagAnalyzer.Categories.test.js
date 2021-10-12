4384471f0201c78c21fa98a95cf40412e2f8dadd
Alexandr Vishniakov <cyberlight@live.ru>
2021-10-12 05:04:28 +0000

    Added tests for figcaption tag

--- __tests__/TagAnalyzer.Categories.test.js
+++ __tests__/TagAnalyzer.Categories.test.js
@@ -299,4 +299,15 @@ describe('TagAnalyzer::Categories', () => {
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.getCategories(parameter)).toStrictEqual(expected);
   });
+
+  it.each`
+        parameter            | expected
+        ${undefined}         | ${[]}
+        ${'#other-content'}  | ${[]}
+    `('Get categoties by $parameter for "figcaption" tag result: $expected', ({parameter, expected}) => {
+    const tag = rules.figcaption;
+
+    const analyzer = new TagAnalyzer(tag);
+    expect(analyzer.getCategories(parameter)).toStrictEqual(expected);
+  });
 });
--- __tests__/TagAnalyzer.ContentModel.test.js
+++ __tests__/TagAnalyzer.ContentModel.test.js
@@ -331,4 +331,15 @@ describe('TagAnalyzer::ContentModel', () => {
     const analyzer = new TagAnalyzer(tag);
     expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
   });
+
+  it.each`
+        parameter                     | expected
+        ${'#flow-content-2'}          | ${true}
+        ${'#other-content'}           | ${false}
+    `('Can include $parameter to "figcaption" tag result: $expected', ({parameter, expected}) => {
+    const tag = rules.figcaption;
+
+    const analyzer = new TagAnalyzer(tag);
+    expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
+  });
 });
