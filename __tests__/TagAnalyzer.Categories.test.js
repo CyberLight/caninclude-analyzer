bc857033416cdb7e759a8420f586ee10bcb1447e
Alexandr Vishniakov <cyberlight@live.ru>
2021-10-27 06:08:23 +0000

    Added tests for b tag

--- __tests__/TagAnalyzer.Categories.test.js
+++ __tests__/TagAnalyzer.Categories.test.js
@@ -347,7 +347,7 @@ describe('TagAnalyzer::Categories', () => {
 
   describe.each(['em', 'strong', 'small', 's', 'cite', 'q', 'dfn',
     'abbr', 'ruby', 'data', 'time', 'code', 'var', 'samp', 'kbd', 'sub',
-    'sup', 'i'])(
+    'sup', 'i', 'b'])(
       'For tag=%s', (tagName) => {
         it.each`
         parameter                 | expected
--- __tests__/TagAnalyzer.ContentModel.test.js
+++ __tests__/TagAnalyzer.ContentModel.test.js
@@ -408,7 +408,7 @@ describe('TagAnalyzer::ContentModel', () => {
   });
 
   describe.each(['em', 'strong', 'small', 's', 'cite', 'q', 'abbr',
-    'data', 'code', 'var', 'samp', 'kbd', 'sub', 'sup', 'i'])(
+    'data', 'code', 'var', 'samp', 'kbd', 'sub', 'sup', 'i', 'b'])(
       'For tag=%s', (tagName) => {
         it.each`
         parameter                | expected
