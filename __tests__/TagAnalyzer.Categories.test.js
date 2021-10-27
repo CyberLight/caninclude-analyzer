fddc5486f88cb179bf4747bee9ccf5e2bada8703
Alexandr Vishniakov <cyberlight@live.ru>
2021-10-27 06:03:04 +0000

    Added tests for var tag

--- __tests__/TagAnalyzer.Categories.test.js
+++ __tests__/TagAnalyzer.Categories.test.js
@@ -345,7 +345,7 @@ describe('TagAnalyzer::Categories', () => {
     expect(analyzer.getCategories(parameter)).toStrictEqual(expected);
   });
 
-  describe.each(['em', 'strong', 'small', 's', 'cite', 'q', 'dfn', 'abbr', 'ruby', 'data', 'time', 'code'])(
+  describe.each(['em', 'strong', 'small', 's', 'cite', 'q', 'dfn', 'abbr', 'ruby', 'data', 'time', 'code', 'var'])(
       'For tag=%s', (tagName) => {
         it.each`
         parameter                 | expected
--- __tests__/TagAnalyzer.ContentModel.test.js
+++ __tests__/TagAnalyzer.ContentModel.test.js
@@ -407,7 +407,7 @@ describe('TagAnalyzer::ContentModel', () => {
     expect(analyzer.canIncludeParam(parameter)).toStrictEqual(expected);
   });
 
-  describe.each(['em', 'strong', 'small', 's', 'cite', 'q', 'abbr', 'data', 'code'])(
+  describe.each(['em', 'strong', 'small', 's', 'cite', 'q', 'abbr', 'data', 'code', 'var'])(
       'For tag=%s', (tagName) => {
         it.each`
         parameter                | expected
