01f5c0861a1c8c9dd5c5447f624a1613f385e616
Alexandr Vishniakov <cyberlight@live.ru>
2021-11-11 16:44:04 +0600

    Added new tests for CanincludeAnalyzer

---
+++ __tests__/CanincludeAnalyzer.test.js
@@ -0,0 +1,34 @@
+/* eslint-disable max-len */
+const {CanincludeAnalyzer} = require('../index.js');
+const analyzer = new CanincludeAnalyzer();
+
+describe('TagAnalyzer::ContentModel', () => {
+  beforeAll(() => {
+    return analyzer.load();
+  });
+
+  it.each`
+        child              |  parent                | expected
+        ${{name: 'audio'}} |  ${{name: 'video'}}    | ${false}
+        ${{name: 'video'}} |  ${{name: 'audio'}}    | ${false}
+    `('Can include $child to $parent tag result: $expected', ({parent, child, expected}) => {
+    expect(analyzer.canInclude(child, parent)).toStrictEqual(expected);
+  });
+
+  it.each`
+        child              |  parent                | expected
+        ${{name: 'input'}} |  ${{name: 'label'}}    | ${true}
+    `('Can include $child to $parent tag result: $expected', ({parent, child, expected}) => {
+    expect(analyzer.canInclude(child, parent)).toStrictEqual(expected);
+  });
+
+
+  it.each`
+        child                                                    | parent              | expected
+        ${{name: 'a', params: ['hasAttr:#attr-hyperlink-href']}} | ${{name: 'button'}} | ${false}
+        ${{name: 'a'}}                                           | ${{name: 'button'}} | ${true}
+    `('Can include $child to $parent tag result: $expected', ({parent, child, expected}) => {
+    expect(analyzer.canInclude(child, parent)).toStrictEqual(expected);
+  });
+});
+
